6d186b2f8b57ba6315a4dae522ae52d55f772ce1
faryun <43097367+faryun@users.noreply.github.com>
2024-02-25 07:34:10 +0900

    4740

--- 거울_오_거울.cpp
+++ 거울_오_거울.cpp
@@ -7,11 +7,12 @@ int main()
     while (true)
     {
         string str;
-        cin >> str;
+        string result;
+        getline(cin,str);
         if(str == "***") break;
 
-        for(int i = str.length(); i <= 0; i--) cout << str[i];
-        cout << endl;
+        for(int i = str.length() - 1; i >= 0; i--) result += str[i];
+        cout << result << endl;
     }
 
     return 0;
