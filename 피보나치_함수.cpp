67827f49a020d154615da13680a067515b136954
faryun <43097367+faryun@users.noreply.github.com>
2024-02-29 05:56:18 +0900

    피보나치 함수

--- 피보나치_함수.cpp
+++ 피보나치_함수.cpp
@@ -2,26 +2,35 @@
 
 using namespace std;
 
+int a = 0, b = 0;
 int fibonacci(int n) {
     if (n == 0) {
-        printf("0");
+        a++;
         return 0;
-    } else if (n == 1) {
-        printf("1");
+    } 
+    else if (n == 1) {
+        b++;
         return 1;
-    } else {
+    } 
+    else {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int n,num;
     cin >> n;
     for(int i = 0; i < n; i++)
     {
+        a = 0;
+        b = 0;
         cin >> num;
-        cout << fibonacci(num) << endl;
+        fibonacci(num);
+        cout << a << " " << b << '\n';
     }
     return 0;
 }
