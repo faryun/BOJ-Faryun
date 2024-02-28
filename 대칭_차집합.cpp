3a26a1bbd4c6a9fdae5a57737f24ee4d2d20cfee
faryun <43097367+faryun@users.noreply.github.com>
2024-02-29 05:29:49 +0900

    1269

--- 대칭_차집합.cpp
+++ 대칭_차집합.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <set>
 #include <algorithm>
 
 using namespace std;
@@ -9,9 +9,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    vector<int> A;
-    vector<int> B;
-    vector<int> C;
+    set<int> A;
+    set<int> B;
+    set<int> C;
 
     int a, b, num;
     cin >> a >> b;
@@ -19,17 +19,15 @@ int main()
     for(int i = 0; i < a; i++)
     {
         cin >> num;
-        A.push_back(num);
+        A.insert(num);
     }
 
     for(int j = 0; j < b; j++)
     {
         cin >> num;
-        B.push_back(num);
+        B.insert(num);
     }
     
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end());
     set_difference(A.begin(),A.end(),B.begin(),B.end(),inserter(C,C.begin()));
     set_difference(B.begin(),B.end(),A.begin(),A.end(),inserter(C,C.begin()));
     
