c5a3b95b8868a0833ae7218c6d111784664f04c5
faryun <43097367+faryun@users.noreply.github.com>
2024-03-08 06:22:47 +0900

    1463

---
+++ 1_로_만들기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> num;
+    vector<int> result;
+    int n;
+    cin >> n;
+    result.resize(1000001);
+
+    //백만까지의 배열
+    for(int i = 1; i <= 1000000; i++) num.push_back(i);
+    
+    if();
+    cout << result[n];
+    return 0;
+}
