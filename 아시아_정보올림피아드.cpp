7b0cd5dadc245f98558b8386cb573c7008618adf
faryun <43097367+faryun@users.noreply.github.com>
2024-03-10 16:54:42 +0900

    2535

---
+++ 아시아_정보올림피아드.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int entry;
+    cin >> entry;
+    vector<vector<int>> scoreboard;
+    for(int i = 0; i < entry; i++)
+    {
+        int contry, student, score;
+        cin >> contry >> student >> score;
+    }
+    return 0;
+}
