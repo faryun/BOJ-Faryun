64156d282d7e10b4b72e1c24d293d261167b596c
faryun <43097367+faryun@users.noreply.github.com>
2024-03-27 05:53:53 +0900

    19637

--- IF문_좀_대신_써줘.cpp
+++ IF문_좀_대신_써줘.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+    int n, m;
+    cin >> n >> m;
+    vector<pair<string, int>> power;
+    
+    for(int i = 0; i < n; i++)
+    {
+        string str;
+        int pow;
+        cin >> str >> pow;
+        power.push_back(make_pair(str,pow));
+    }
+    
+    for(int o = 0; o < m; o++)
+    {
+        int temp;
+        cin >> temp;
 
+        for(int p = 0; p < power.size(); p++)
+        {
+            if(temp <= power[p].second)
+            {
+                cout << power[p].first << '\n';
+                break;
+            }
+        }
+    }
+
+    return 0;
 }
