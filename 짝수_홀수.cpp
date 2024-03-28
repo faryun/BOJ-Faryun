60b4ad59283676109a283d9e8ae72a8d4057269c
faryun <43097367+faryun@users.noreply.github.com>
2024-03-29 06:13:48 +0900

    11815

--- 짝수_홀수.cpp
+++ 짝수_홀수.cpp
@@ -6,24 +6,22 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int T;
     cin >> T;
+
     for(int t = 0; t < T; t++)
     {
         long long int n;
-        long long int temp = 0;
-        vector<long long int> result;
         cin >> n;
-        for(long long int i = 1; i <= sqrt(n); i++)
-        {
-            if(n % i == 0) 
-            {
-                result.push_back(i);
-                if(i != n / i) result.push_back(n / i);
-            }
-        }
-        if(result.size() % 2 != 0) cout << 1 << " ";
+        long long int temp;
+        temp = sqrtl(n);
+
+        if(sqrtl(n) == temp) cout << 1 << " ";
         else cout << 0 << " ";
-    }    
+    }
+
     return 0;
 }
