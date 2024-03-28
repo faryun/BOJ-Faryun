5e27eaa240b140c59418841e237a923a054ca2d1
faryun <43097367+faryun@users.noreply.github.com>
2024-03-29 05:35:58 +0900

    11815

--- 짝수_홀수.cpp
+++ 짝수_홀수.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    
+    int T;
+    cin >> T;
+    for(int t = 0; t < T; t++)
+    {
+        long long int n;
+        long long int temp = 0;
+        vector<long long int> result;
+        cin >> n;
+        for(long long int i = 1; i <= sqrt(n); i++)
+        {
+            if(n % i == 0) 
+            {
+                result.push_back(i);
+                if(i != n / i) result.push_back(n / i);
+            }
+        }
+        if(result.size() % 2 != 0) cout << 1 << " ";
+        else cout << 0 << " ";
+    }    
     return 0;
 }
