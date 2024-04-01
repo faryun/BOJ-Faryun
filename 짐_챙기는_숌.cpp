9ca1550e33b733e18824a5c39545347197e837a4
faryun <43097367+faryun@users.noreply.github.com>
2024-04-01 09:34:09 +0900

    1817

---
+++ 짐_챙기는_숌.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    deque<int> books;
+    int box, book;
+    int cnt = 0;
+    int sum;
+    int result = 1;
+
+    cin >> book >> box;
+
+    for(int i = 0; i < book; i++)
+    {
+        int input;
+        cin >> input;
+        books.push_back(input);
+    }
+    
+    while (true)
+    {
+        if(books.empty()) break;
+
+        if(sum + books[0] > box)
+        {
+            sum = 0;
+            result++;
+        }
+
+        else
+        {
+            sum += books[0];
+            books.pop_front();
+        }
+    }
+
+    if(book != 0) cout << result;
+    else cout << 0;
+    
+    return 0;
+}
