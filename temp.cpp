6a5bdc7506fbd8f4fa2aacc7755435ea0b096a00
faryun <43097367+faryun@users.noreply.github.com>
2024-04-10 18:40:30 +0900

    13552

--- temp.cpp
+++ temp.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct point
+{
+    int px;
+    int py;
+    int pz;
+};
+
 int main()
 {
-    int a, b, c;
-    int max, sum;
-    while (true)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<point> p;
+    int n;
+    cin >> n;
+    p.resize(n);
+    for(int i = 0; i < n; i++)
     {
-        cin >> a >> b >> c;
-        if(a == 0 && b == 0 && c == 0) break;
-        
-        if(a > b && a > c)
-        {
-            max = a;
-            sum = b+c;
-        }
-        
-        else if (b > c)
-        {
-            max = b;
-            sum = a+c;
-        }
-
-        else
-        {
-            max = c;
-            sum = a+b;
-        }
-
-        if(max > sum) cout << "Invalid" << '\n';
-        else if(a != b && b != c && a != c) cout << "Isosceles" << '\n';
-        else if((a == b && b == c) || (a == c && b == c) || (a == b && a == c)) cout << "Isosceles" << '\n';
-        else cout << "Equilateral" << '\n';
+        int x, y, z;
+        cin >> x >> y >> z;
+        p[i].px = x;
+        p[i].py = y;
+        p[i].pz = z;
     }
-     
+
+    int b;
+    cin >> b;
+
+    for(int r = 0; r = b; r++)
+    {
+        int result = 0;
+        int bx, by, bz, rad;
+        cin >> bx >> by >> bz >> rad;
+        cout << result << '\n';
+    }
     return 0;
 }
