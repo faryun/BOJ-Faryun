2c8de981f500e6015bb7355aef6866a2f7bf9d1e
faryun <43097367+faryun@users.noreply.github.com>
2024-04-12 12:47:29 +0900

    17177

---
+++ 내접사각형_만들기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ab, cd, ad;
+    int result = 0;
+    cin >> ab >> cd >> ad;
+    int dia = ab;
+    cout << result;
+    return 0;
+}
