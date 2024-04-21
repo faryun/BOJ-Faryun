8ec05c821b48499f04b92a7a4600c8a33923cc44
faryun <43097367+faryun@users.noreply.github.com>
2024-04-21 11:06:55 +0900

    15828

--- Router.cpp
+++ Router.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <deque>
 
 using namespace std;
 
 int main()
 {
+    int size;
+    cin >> size;
+    deque<int> router;
+
+    while (true)
+    {
+        int n;
+        cin >> n;
+        if(n == -1) 
+        {
+            if(router.empty()) cout << "empty";
+            else for(auto o : router) cout << o << " ";
+            break;
+        }
+        if(n == 0) router.pop_front(); 
+        else
+        {
+            if(router.size() < size) router.push_back(n);
+        }
+    }
     
     return 0;
 }
