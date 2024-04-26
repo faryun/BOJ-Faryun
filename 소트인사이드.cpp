d82224a0f2e9b3803dedffc43fc8ba1225bcb797
faryun <43097367+faryun@users.noreply.github.com>
2024-04-26 16:47:07 +0900

    1427

--- 소트인사이드.cpp
+++ 소트인사이드.cpp
@@ -8,8 +8,9 @@ int main()
 {
     string str;
     int num;
+    cin >> num;
     str = to_string(num);
-    sort(str.begin(),str.end());
+    sort(str.begin(),str.end(),greater<>());
     cout << str << endl;
     return 0;
 }
