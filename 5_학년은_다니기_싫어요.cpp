67df31292d33131c0716d7b020b6575e4fda0589
faryun <43097367+faryun@users.noreply.github.com>
2024-05-07 13:35:39 +0900

    23028

---
+++ 5_학년은_다니기_싫어요.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    int needa = 66 - a;
+    int needb = 130 - b;
+    cout << needa << " " << needb << endl;
+
+    for(int i = n; i < 9; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+    }
+    //cout << "Nice";
+    //cout << "Nae ga wae";
+    return 0;
+}
