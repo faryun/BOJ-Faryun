b43bc631974efaa5b59c801c33b822010f1e9610
faryun <43097367+faryun@users.noreply.github.com>
2024-05-07 19:33:42 +0900

    4383

---
+++ 점프는_즐거워.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        vector<int> num;
+        vector<int> temp;
+        bool yn = true;
+        int n;
+        cin >> n;
+        if(cin.eof()) break;
+        for(int i = 0; i < n; i++)
+        {
+            double a;
+            cin >> a;
+            num.push_back(a);
+        }
+        
+        if(num.size() > 1)
+        {
+            for(int j = 0; j < n - 1; j++) temp.push_back(abs(num[j] - num[j+1]));
+
+            sort(temp.begin(),temp.end());
+
+            for(int l = 1; l <= n - 1; l++)
+            {
+                if(l != temp[l-1])
+                {
+                    yn = false;
+                    break;
+                }
+            }
+        }
+
+        if(yn) cout << "Jolly" << '\n';
+        else cout << "Not jolly" << '\n';
+    }
+    
+    return 0;
+}
