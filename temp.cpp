dfb244d1ae4599d3e69e02d41d2cf3b6e69fe865
faryun <43097367+faryun@users.noreply.github.com>
2024-05-19 11:37:31 +0900

    15720

--- temp.cpp
+++ temp.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
-#include <array>
-
+#include <vector>
 using namespace std;
+
+struct cow
+{
+    int burger = 0;
+    int side = 0;
+    int drink = 0;
+};
+
 
 int main()
 {
-    array<int, 4> meter = {40, 30, 20, 10};
-    int m;
-    cin >> m;
-
-    for(int i = 0; i < meter.size(); i++)
+    vector<cow> food;
+    food.resize(1001);
+    int b, c, d;
+    cin >> b >> c >> d;
+    for(int i = 0; i < b; i++)
     {
-        if(m % meter[i] == 0)
-        {
-            cout << 4 - i;
-            break;
-        }
-
-        else cout << 1;
+        int bu;
+        cin >> bu;
+        food[i].burger = bu;
     }
+    
+    for(int i = 0; i < c; i++)
+    {
+        int bu;
+        cin >> bu;
+        food[i].side = bu;
+    }
+
+    for(int i = 0; i < d; i++)
+    {
+        int bu;
+        cin >> bu;
+        food[i].drink = bu;
+    }
+
+    for(auto t : food) cout << t.burger << " " << t.side << " " << t.drink << endl;
     return 0;
 }
