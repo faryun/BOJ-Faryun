bafe52f9c92bcc998b362f128d7c39b1bfc29855
faryun <43097367+faryun@users.noreply.github.com>
2024-06-01 07:13:45 +0900

    18110

--- solved_ac.cpp
+++ solved_ac.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
-#include <list>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-    int t, n, sum;
-    list<int> score;
+    int t, n;
+    int sum = 0;
+    vector<int> score;
     cin >> t;
 
-    for(int i = 0; i<t; i++)
+    for(int i = 0; i < t; i++)
     {
         cin >> n;
         score.push_back(n);
     }
     
-    score.sort();
+    sort(score.begin(), score.end());
 
-    for(auto o : score)
+    double temp = t * 0.15;
+    temp = floor(temp + 0.5);
+
+    if(score.empty()) cout << 0;
+    else
     {
-        sum += o;
-    }
+        for(int o = temp; o < t - temp; o++)
+        {
+            sum += score[o];
+        }
 
-    cout << sum / t << endl;
+        cout << floor((sum / (t - (temp + temp))) + 0.5);
+    }
     return 0; 
 }
