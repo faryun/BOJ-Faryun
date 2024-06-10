15df883ce84a8344d59d70102de0c3ee9258907a
faryun <43097367+faryun@users.noreply.github.com>
2024-06-10 14:09:42 +0900

    2752

--- 세수정렬.cpp
+++ 세수정렬.cpp
@@ -10,5 +10,24 @@ int main()
     {
         cin >> num[i];
     }
+
+    int temp;
+    //오름차순 정렬
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            //j의 값이 j+1의 값보다 크다면 바꾸기
+            if(num[j] > num[j+1])
+            {
+                temp = num[j];
+                num[j] = num[j+1];
+                num[j+1] = temp;
+            }
+        }
+    }
+
+    for(int o = 0; o < 3; o++) cout << num[o] << " ";
+    
     return 0;
 }
