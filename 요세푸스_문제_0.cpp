97f795b2666cb52cafffa2ed9a06f2e1b4eb1078
faryun <43097367+faryun@users.noreply.github.com>
2024-06-18 12:01:44 +0900

    11866

--- 요세푸스_문제_0.cpp
+++ 요세푸스_문제_0.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
-#include <list>
+#include <deque>
+#include <vector>
+
 using namespace std;
 
 int main()
 {
     //입력
     int n, k;
-    list<int> num;
+    deque<int> num;
+    vector<int> result;
     cin >> n >> k;
     for(int i = 1; i <= n; i++) num.push_back(i);
     
-    /*
-        1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 
-        이제 순서대로 K번째 사람을 제거한다. 
-        한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 
-        이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 
-        원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다. 
-        예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
-        1 2 3 4 5 6 7
-    */
+    for(int j = 0; j < n; j++)
+    {
+        for(int r = 0; r < k - 1; r++)
+        {
+            int temp = *num.begin();
+            num.pop_front();
+            num.push_back(temp);
+        }
+        int temp = *num.begin();
+        num.pop_front();
+        result.push_back(temp);
+    }
     
+    cout << '<';
+    for(int o = 0; o < result.size(); o++)
+    {
+        if(o < result.size()-1) cout << result[o] << ", ";
+        else cout << result[o];
+    }
+    cout << '>';
     return 0;
 }
