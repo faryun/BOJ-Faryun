83f5e91e4326e3d81b096f684863452f1cf585ee
faryun <43097367+faryun@users.noreply.github.com>
2024-06-24 10:28:44 +0900

    10816

--- 숫자_카드_2.cpp
+++ 숫자_카드_2.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main()
 {
-    int num[500000], compare[500000];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    map<int, int> num;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int in;
+        cin >> in;
+        if(num.find(in) != num.end())
+        {
+            auto temp = num.find(in);
+            temp->second++;
+        }
+        else num.insert(make_pair(in, 1));
+    }
 
+    int T;
+    cin >> T;
+
+    for(int t = 0; t < T; t++)
+    {
+        int o;
+        cin >> o;
+        cout << num[o] << " ";
+    }
     return 0;
 }
