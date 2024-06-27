4091755b9f4131ecbb1d0186939b011862eca7e5
faryun <43097367+faryun@users.noreply.github.com>
2024-06-28 08:12:43 +0900

    1264

--- 모음의_개수.cpp
+++ 모음의_개수.cpp
@@ -6,18 +6,21 @@ int main()
 {
     string str;
 
-    int result = 0;
     while (true)
     {
-        cin >> str;
+        int result = 0;
+        getline(cin,str);
         if(str == "#") break;
 
         for(int i = 0; i < str.length(); i++)
         {
-            if(str[i] == 'a'|| str[i] == 'e'|| str[i] == 'i' || str[i] == 'o'|| str[i] == 'u') result++;
+            if(str[i] == 'a'|| str[i] == 'e'|| str[i] == 'i' || str[i] == 'o'|| str[i] == 'u'
+            || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') 
+            result++;
         }
+        
+        cout << result << endl;
     }
-    cout << result << endl;
     
     return 0;
 }
