40b7dbe980c4142e0b69f75088c54f5faa1c4ea7
faryun <43097367+faryun@users.noreply.github.com>
2024-07-04 08:22:06 +0900

    7891

---
+++ Can_you_add_this.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    for(int i = 0; i < T; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
