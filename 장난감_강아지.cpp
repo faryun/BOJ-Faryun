c85dbe438402e64b5e98ac2eb7b9254ba90f67d5
faryun <43097367+faryun@users.noreply.github.com>
2024-07-10 09:26:41 +0900

    31287

--- 장난감_강아지.cpp
+++ 장난감_강아지.cpp
@@ -4,11 +4,30 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int s, k;
     int x = 0, y = 0;
+    bool flag = false;
     string str;
     cin >> s >> k;
     cin >> str;
-    cout << str + str << endl;
+    
+    for(int i = 0; i < k; i++)
+    {
+        for(int j = 0; j < s; j++)
+        {
+            if(str[j] == 'U') y++;
+            else if(str[j] == 'D') y--;
+            else if(str[j] == 'L') x--;
+            else if(str[j] == 'R') x++;
+
+            if(x == 0 && y == 0) flag = true;
+        }
+    }
+
+    if(flag) cout << "YES";
+    else cout << "NO";
     return 0;
 }
