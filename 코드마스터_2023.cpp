7d0bc92890f3c545679ff978381864707a1a84a9
faryun <43097367+faryun@users.noreply.github.com>
2024-07-26 13:07:55 +0900

    28235

---
+++ 코드마스터_2023.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    if(str == "SONGDO") cout << "HIGHSCHOOL";
+    else if(str == "CODE") cout << "MASTER";
+    else if(str == "2023") cout << "0611";
+    else if(str == "ALGORITHM") cout << "CONTEST";
+    return 0;
+}
