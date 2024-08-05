b4c78cef146072b702a71ea181affeff3f48f36c
faryun <43097367+faryun@users.noreply.github.com>
2024-08-05 20:29:11 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    if(a == 'n' || a == 'N') cout << "Naver D2";
+    else cout << "Naver Whale";
+    return 0;
+}
