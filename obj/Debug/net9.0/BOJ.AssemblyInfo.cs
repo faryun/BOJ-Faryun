c48aea17ccc58e7bcd57b5e1ee3363b65bd96ddf
faryun <43097367+faryun@users.noreply.github.com>
2024-12-05 14:09:42 +0900

    update gitignore

--- obj/Debug/net9.0/BOJ.AssemblyInfo.cs
+++ obj/Debug/net9.0/BOJ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BOJ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4b592f89b35feeb7d68afd36315f064feef84c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67cc84d6bc768cb49a80acb69c35aef399e3e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BOJ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BOJ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
