e687d86e322fe0be080781331cebdb8fe5c0b632
faryun <43097367+faryun@users.noreply.github.com>
2024-12-24 15:15:55 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.cs
@@ -0,0 +1,6 @@
+string str = Console.ReadLine();
+if(str == "fdsajkl;" || str == "jkl;fdsa") Console.Write("in-out");
+else if(str == "asdf;lkj" || str == ";lkjasdf") Console.Write("out-in");
+else if(str == "asdfjkl;") Console.Write("stairs");
+else if(str == 	";lkjfdsa") Console.Write("reverse");
+else Console.Write("molu");
