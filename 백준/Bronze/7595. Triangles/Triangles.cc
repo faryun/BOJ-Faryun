bbd44f1bc3605b49735a1ba4b11c0fc97491465f
faryun <43097367+faryun@users.noreply.github.com>
2025-01-10 16:06:08 +0900

    [Bronze IV] Title: Triangles, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        int n;
+        cin >> n;
+        if(n == 0) break;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                cout << "*";
+            }
+            cout << "\n";
+        }
+    }
+}
